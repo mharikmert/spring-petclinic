8f503c1365b3c9b937a0e4a707741f203cb336dd
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-09-24 11:42:55 +0300

    deletion

--- src/main/java/com/spring/petclinic/Exceptions/OwnerNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.spring.petclinic.Exceptions;
-public class OwnerNotFoundException extends RuntimeException{
-    public OwnerNotFoundException(String message){
-        System.out.println("Owner not found..");
-    }
-}
