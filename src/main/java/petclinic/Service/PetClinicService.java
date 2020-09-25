970531f1531a2d405c591076a581bece4ce390f7
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-09-25 17:42:58 +0300

    uncssy

--- src/main/java/petclinic/Service/PetClinicService.java
+++ src/main/java/petclinic/Service/PetClinicService.java
@@ -5,6 +5,7 @@ import petclinic.Exceptions.OwnerNotFoundException;
 import petclinic.Model.Owner;
 import java.util.List;
 public interface PetClinicService {
+
     List<Owner> findOwners();
     List<Owner> findOwners(String lastName);
     Owner findOwner (Long id) throws OwnerNotFoundException;
