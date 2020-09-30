5cd22ffa11f56a72937bbb06512707d8e5d71f2b
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-09-30 16:08:16 +0300

    adding login page access and remember me feature in configuration

--- src/main/java/petclinic/SecurityConfiguration.java
+++ src/main/java/petclinic/SecurityConfiguration.java
@@ -1,11 +1,17 @@
 package petclinic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
@@ -14,11 +20,16 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
                         "/css/**",
                         "/js/**",
                         "/images/**",
-                        "webjars/**").permitAll()
+                        "webjars/**",
+                        "/login.html").permitAll()
                 /* Request methods are accessed by just authenticated users*/
                 .anyRequest().authenticated();
 
         /* unauthenticated users redirect login page by default*/
-        http.formLogin();
+        http.formLogin()
+            .loginPage("/login.html") //login path
+            .loginProcessingUrl("/login") //submit uri
+            .failureForwardUrl("/login.html?loginFailed=true"); //authentication failure
+        http.rememberMe().userDetailsService(userDetailsService); //remember be ability
     }
 }
