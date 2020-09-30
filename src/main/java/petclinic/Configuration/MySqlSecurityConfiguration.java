1fc06cda19cdaed4b5f8f9079845a4bc26abf38d
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-09-30 17:42:02 +0300

    database security configuration file to access database from the port

--- src/main/java/petclinic/Configuration/MySqlSecurityConfiguration.java
+++ src/main/java/petclinic/Configuration/MySqlSecurityConfiguration.java
@@ -1,2 +1,17 @@
-package petclinic.Configuration;public class MySqlSecurityConfiguration {
+package petclinic.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(value = 0) //to enable this class first, normally can't access to the console
+public class MySqlSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/h2-console/**").authorizeRequests().anyRequest().permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
 }
