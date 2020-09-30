8f60c68cb7d15b617328f610ecd1ea4d5bee0efa
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-09-30 12:49:49 +0300

    removing enable schedulin annotaion

--- src/main/java/petclinic/Application.java
+++ src/main/java/petclinic/Application.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @ServletComponentScan
 @SpringBootApplication
 @EnableConfigurationProperties(Properties.class)
