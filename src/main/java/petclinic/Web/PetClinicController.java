f81f8d9b5940a367de525f6f1f16c4deaba51d2a
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-09-30 14:12:48 +0300

    to view logout page in petclinicControler

--- src/main/java/petclinic/Web/PetClinicController.java
+++ src/main/java/petclinic/Web/PetClinicController.java
@@ -3,20 +3,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import petclinic.Service.PetClinicService;
 
 @Controller
 @Configuration
 public class PetClinicController {
-	@Autowired
+
     private PetClinicService petClinicService;
 
-    @RequestMapping ("/owners")
-    public ModelAndView getOwners(){
+    @Autowired
+    public void setPetClinicService(PetClinicService petClinicService){
+        this.petClinicService = petClinicService;
+    }
+
+    @RequestMapping("/owners")
+    public ModelAndView getOwners() {
         ModelAndView mav = new ModelAndView();
-        mav.addObject("owners",petClinicService.findOwners());
+        mav.addObject("owners", petClinicService.findOwners());
         mav.setViewName("owners");
+        return mav;
+    }
+
+    @RequestMapping(value =  {"/","index.html"})
+    public ModelAndView index(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index");
         return mav;
     }
 }
