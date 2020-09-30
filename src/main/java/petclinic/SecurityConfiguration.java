3b1462d00d42c5e180708ce4d4a9255f548fe2d8
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-09-30 12:52:21 +0300

    creating a securtiy class in petclinic package

--- src/main/java/petclinic/SecurityConfiguration.java
+++ src/main/java/petclinic/SecurityConfiguration.java
@@ -1,4 +1,24 @@
 package petclinic;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                /* open sources path's for any user*/
+//                .antMatchers("/**/favicon.ico",
+//                        "/css/**",
+//                        "/js/**",
+//                        "/images/**",
+//                        "webjars/**").permitAll()
+                /* Request methods are accessed by just authenticated users*/
+                .anyRequest().authenticated();
+
+        /* unauthenticated users redirects login page by default*/
+        http.formLogin();
+    }
 }
