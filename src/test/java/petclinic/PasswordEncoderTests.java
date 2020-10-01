dc9591298cee135f89600b699158f0c0825d21b7
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-10-01 15:39:14 +0300

    encoding passwords with bcrypt method in test class

--- src/test/java/petclinic/PasswordEncoderTests.java
+++ src/test/java/petclinic/PasswordEncoderTests.java
@@ -1,2 +1,14 @@
-package petclinic;public class PasswordEncoderTests {
+package petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTests {
+    @Test
+    public void passwordEncryptionTest(){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println("{bcrypt}" + encoder.encode("password"));
+        System.out.println("{bcrypt}" + encoder.encode("password"));
+        System.out.println("{bcrypt}" + encoder.encode("password"));
+    }
 }
