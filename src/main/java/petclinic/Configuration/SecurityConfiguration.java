9ccdc96875f6a4311ee662aac0459a12489f3859
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-10-01 15:37:18 +0300

    uncssry

--- src/main/java/petclinic/Configuration/SecurityConfiguration.java
+++ src/main/java/petclinic/Configuration/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.sql.DataSource;
