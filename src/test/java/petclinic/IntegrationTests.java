781a1400647256b8f4b88a3bf407079af51997f7
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-10-06 16:14:29 +0300

    integration tests trials

--- src/test/java/petclinic/IntegrationTests.java
+++ src/test/java/petclinic/IntegrationTests.java
@@ -2,17 +2,13 @@ package petclinic;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import petclinic.Service.PetClinicService;
 import petclinic.Model.Owner;
 
 import java.util.List;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest(properties = {"spring.profiles.active = dev"})
 public class IntegrationTests {
     @Autowired
