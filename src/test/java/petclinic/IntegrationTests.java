195d86d94f208b89b25401b1d0ac6734670a4703
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-10-06 09:19:29 +0300

    integration test class and tested findOwners method

--- src/test/java/petclinic/IntegrationTests.java
+++ src/test/java/petclinic/IntegrationTests.java
@@ -1,4 +1,27 @@
 package petclinic;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import petclinic.Service.PetClinicService;
+import petclinic.Model.Owner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = {"spring.profiles.active = dev"})
 public class IntegrationTests {
+    @Autowired
+    private PetClinicService petClinicService;
+
+    @Test
+    public void findOwners(){
+        List<Owner> owners = petClinicService.findOwners();
+        MatcherAssert.assertThat(owners.size(), Matchers.equalTo(10));
+    }
+
 }
