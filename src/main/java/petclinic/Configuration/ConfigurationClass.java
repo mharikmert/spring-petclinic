865a6bcbe8c480948392650100b679e34bd20fd4
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-10-06 16:15:10 +0300

    creating restTemplate bean in configurationClass

--- src/main/java/petclinic/Configuration/ConfigurationClass.java
+++ src/main/java/petclinic/Configuration/ConfigurationClass.java
@@ -1,7 +1,9 @@
 package petclinic.Configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import petclinic.Properties;
 
 import javax.annotation.PostConstruct;
@@ -16,5 +18,9 @@ public class ConfigurationClass {
     @PostConstruct // see the method content while initializing
     public void anyName(){
         System.out.println("is the value of newProperty is still false? : " + getProperty.isNewProperty());
+    }
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
