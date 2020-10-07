374a826554ddc6c7a18044c7f83db1d072c5891b
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-10-07 16:27:11 +0300

    new integration test class with ownerRepository

--- src/test/java/petclinic/DAO/OwnerRepositoryTests.java
+++ src/test/java/petclinic/DAO/OwnerRepositoryTests.java
@@ -1,2 +1,52 @@
-package petclinic.DAO;public class OwnerRepositoryTests {
+package petclinic.DAO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import petclinic.Model.Owner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+@Transactional // adding transaction ability to test class
+public class OwnerRepositoryTests {
+    @Autowired
+    private OwnerRepository ownerRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test
+    public void findOwners(){
+            ownerRepository.findAll();
+    }
+
+    @Test @Rollback
+    public void deleteOwner(){
+           ownerRepository.deleteOwner(10L);
+    }
+
+    @Test @Commit
+    public void testCreateOwner(){
+        Owner owner = new Owner("test","user", 10L);
+        Owner owner2 = new Owner();
+        owner2.setFirstName(null);
+        owner2.setLastName(null);
+        ownerRepository.createOwner(owner);
+        //Assertions.assertThrows(SQLGrammarException.class,() -> {
+    //ownerRepository.createOwner(owner);});
+    }
+    @Test //rollback default
+    public void testUpdateOwner(){
+        Owner owner =  new Owner("updated", "owner", 10L);
+        ownerRepository.updateOwner(owner);
+    }
+
 }
