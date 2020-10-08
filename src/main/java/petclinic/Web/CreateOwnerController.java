d71f38cfad51352d697d61bb6ce767ac132318cd
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-10-08 17:31:46 +0300

    new controller class created for createOwner page

--- src/main/java/petclinic/Web/CreateOwnerController.java
+++ src/main/java/petclinic/Web/CreateOwnerController.java
@@ -1,2 +1,44 @@
-package petclinic.Web;public class CreateOwnerController {
+package petclinic.Web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import petclinic.Model.Owner;
+import petclinic.Service.PetClinicService;
+
+@Controller
+public class CreateOwnerController {
+    @Autowired
+    private PetClinicService petClinicService;
+
+    @RequestMapping(value = "/owners/createOwner", method = RequestMethod.GET)
+    public String createOwner (){
+        return "createOwner";
+    }
+
+    @ModelAttribute
+    public Owner initModel(){
+        return new Owner();
+    }
+
+    @RequestMapping(value = "/owners/createOwner", method = RequestMethod.POST)
+    public String handleCreateForm(@ModelAttribute Owner owner){
+        petClinicService.createOwner(owner);
+        return "redirect:/owners";
+    }
+
+    @RequestMapping(value = "/owners/updateOwner", method = RequestMethod.GET)
+    public String updateOwner(){return "updateOwner";}
+
+    @ModelAttribute
+    public Owner updateModel() {return new Owner();}
+    @RequestMapping(value = "/owners/updateOwner", method = RequestMethod.POST)
+    public String handleUpdateForm(@ModelAttribute Owner owner){
+        petClinicService.update(owner);
+        return "redirect:/owners";
+    }
+
 }
