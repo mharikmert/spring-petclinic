71a232d3680ad66f0cc3b152a3b641ff5b53f188
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-10-08 13:26:31 +0300

    testing without authentication with ignoring the exception manually

--- src/test/java/petclinic/Security/PetClinicSecurityWithoutAuthTests.java
+++ src/test/java/petclinic/Security/PetClinicSecurityWithoutAuthTests.java
@@ -1,4 +1,21 @@
 package petclinic.Security;
 
-public class PetClinicSecurityWithourAuthTests {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import petclinic.Service.PetClinicService;
+
+@SpringBootTest(properties = "spring.profiles.active = dev")
+public class PetClinicSecurityWithoutAuthTests {
+    @Autowired
+    private PetClinicService petClinicService;
+
+    @Test
+    public void testFindOwners(){
+        Assertions.assertThrows(AuthenticationCredentialsNotFoundException.class, () -> {//allow to method throws this exception
+            petClinicService.findOwners();
+        });
+    }
 }
