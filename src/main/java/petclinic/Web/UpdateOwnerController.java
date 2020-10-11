b972e09b0d6d01bcf78e41aa54d5ea8a72b85785
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-10-11 21:26:26 +0300

    same changes in updateOwnerController

--- src/main/java/petclinic/Web/UpdateOwnerController.java
+++ src/main/java/petclinic/Web/UpdateOwnerController.java
@@ -3,10 +3,13 @@ package petclinic.Web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import petclinic.Exceptions.OwnerNotFoundException;
 import petclinic.Service.PetClinicService;
 
 import petclinic.Model.Owner;
@@ -18,13 +21,15 @@ public class UpdateOwnerController {
 
     @RequestMapping(value = "/owners/update/{id}", method = RequestMethod.GET)
     public String loadOwner(@PathVariable Long id, ModelMap modelMap){
-        Owner owner = petClinicService.findOwner(id);
+          Owner owner = petClinicService.findOwner(id);
         modelMap.put("owner", owner);
         return "updateOwner";
     }
 
     @RequestMapping(value = "/owners/update/{id}", method = RequestMethod.POST)
-    public String handleUpdateForm(@ModelAttribute Owner owner){
+    public String handleUpdateForm(@ModelAttribute @Validated Owner owner, BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return "updateOwner";
         petClinicService.update(owner);
         return "redirect:/owners";
     }
