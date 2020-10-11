ba9d38fa470bfb7bf4353b7241457d30e3e862f3
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-10-11 21:25:36 +0300

    @Valid annotation added in method declaration and bindingResult

--- src/main/java/petclinic/Web/CreateOwnerController.java
+++ src/main/java/petclinic/Web/CreateOwnerController.java
@@ -2,6 +2,8 @@ package petclinic.Web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,7 +27,9 @@ public class CreateOwnerController {
     }
 
     @RequestMapping(value = "/owners/createOwner", method = RequestMethod.POST)
-    public String handleCreateForm(@ModelAttribute Owner owner){
+    public String handleCreateForm(@ModelAttribute @Validated Owner owner, BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return "createOwner";
         petClinicService.createOwner(owner);
         return "redirect:/owners";
     }
