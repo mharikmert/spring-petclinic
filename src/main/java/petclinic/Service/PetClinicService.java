921195453b1452ce1e0c9ece38925f970183c0e3
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-10-17 19:20:44 +0300

    unncsry adding new methods to the service interface

--- src/main/java/petclinic/Service/PetClinicService.java
+++ src/main/java/petclinic/Service/PetClinicService.java
@@ -3,6 +3,8 @@ package petclinic.Service;
 import petclinic.Exceptions.OwnerNotFoundException;
 
 import petclinic.Model.Owner;
+import petclinic.Model.Vet;
+
 import java.util.List;
 public interface PetClinicService {
 
@@ -12,4 +14,7 @@ public interface PetClinicService {
     void createOwner(Owner owner);
     Owner update(Owner owner);
     void deleteOwner(Long id);
+
+    List<Vet> findVets();
+    Vet findVetById(Long id);
 }
