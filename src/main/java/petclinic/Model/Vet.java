438cfeb016b1d9c4078cd3fdd0d1c7462c4ffb27
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-10-17 19:21:41 +0300

    vet class and data fields

---
+++ src/main/java/petclinic/Model/Vet.java
@@ -0,0 +1,44 @@
+package petclinic.Model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "t_vet")
+public class Vet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NotEmpty
+    @Column(name = "last_name")
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
