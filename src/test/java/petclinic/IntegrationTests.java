15389f62d6ed76ff15248bd515a0f71a902489ed
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-10-17 19:20:24 +0300

    unncsry test for findVets and other vet methods

--- src/test/java/petclinic/IntegrationTests.java
+++ src/test/java/petclinic/IntegrationTests.java
@@ -5,6 +5,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import petclinic.Model.Vet;
 import petclinic.Service.PetClinicService;
 import petclinic.Model.Owner;
 
@@ -18,6 +19,12 @@ public class IntegrationTests {
     public void findOwners(){
         List<Owner> owners = petClinicService.findOwners();
         MatcherAssert.assertThat(owners.size(), Matchers.equalTo(10));
+    }
+
+    @Test
+    public void testFindVets(){
+        List<Vet> vets = petClinicService.findVets();
+        MatcherAssert.assertThat(vets.size(), Matchers.equalTo(3));
     }
 
 }
