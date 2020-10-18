0763bb0c1f8149ebe6d252fbc155ce115a398342
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-10-18 16:35:10 +0300

    vet's id is gone to and extends base entity as super class all the model classes

--- src/main/java/petclinic/Model/Vet.java
+++ src/main/java/petclinic/Model/Vet.java
@@ -5,10 +5,7 @@ import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "t_vet")
-public class Vet {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class Vet extends BaseEntity{
 
     @NotEmpty
     @Column(name = "first_name")
@@ -17,14 +14,6 @@ public class Vet {
     @NotEmpty
     @Column(name = "last_name")
     private String lastName;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getFirstName() {
         return firstName;
