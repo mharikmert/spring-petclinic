e40ceb927b2c0bc6351b6b270a01d76b41803cff
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-10-18 16:32:42 +0300

    abstract base entity class for jpa audition capabilities

--- src/main/java/petclinic/Model/BaseEntity.java
+++ src/main/java/petclinic/Model/BaseEntity.java
@@ -1,2 +1,36 @@
-package petclinic.Model;public class BaseEntity {
+package petclinic.Model;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity{
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private Date createdDate;
+
+    @LastModifiedBy
+    private String updatedBy;
+
+    @LastModifiedDate
+    private Date updatedDate;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    public void setId(Long id){
+        this.id = id;
+    }
+    public Long getId(){
+        return this.id;
+    }
 }
