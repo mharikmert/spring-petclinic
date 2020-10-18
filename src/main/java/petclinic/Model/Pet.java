5ae3a4347cddebc635d8299b35f2dfc5cde54b79
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-10-18 16:34:24 +0300

    pets id is also moved to base entity class

--- src/main/java/petclinic/Model/Pet.java
+++ src/main/java/petclinic/Model/Pet.java
@@ -7,12 +7,7 @@ import java.util.Date;
 
 @Entity
 @Table(name = "t_pet")
-public class Pet {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "petClinicSeqGen")
-    @SequenceGenerator(name = "petClinicSeqGen", sequenceName = "petclinic_sequence")
-    @Column(name = "id")
-    private Long id;
+public class Pet extends BaseEntity{
 
     @Column(name = "name")
     private String name;
@@ -24,21 +19,6 @@ public class Pet {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
-
-    public Pet(){}
-    public Pet(Long id, String name, Date birthDate){
-        this.id  = id;
-        this.name =name;
-        this.birthDate = birthDate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public Owner getOwner() {
         return owner;
