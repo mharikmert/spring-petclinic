40c9588e482f6e21edf68f9d51b3f8222ded81ea
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-10-20 10:36:01 +0300

    new rest dependencies to the library

---
+++ src/main/java/petclinic/Security/petClinicAuditorAware.java
@@ -0,0 +1,15 @@
+package petclinic.Security;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class PetClinicAuditorAware implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return Optional.ofNullable(auth != null ? auth.getName(): null);
+    }
+}
