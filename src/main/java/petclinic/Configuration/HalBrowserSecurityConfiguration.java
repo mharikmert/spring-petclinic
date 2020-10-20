4da780255097d0a493f92c910cba1f79fd8d3373
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-10-20 10:39:50 +0300

    access to the hal browser in directly and configuration

--- src/main/java/petclinic/Configuration/HalBrowserSecurityConfiguration.java
+++ src/main/java/petclinic/Configuration/HalBrowserSecurityConfiguration.java
@@ -1,2 +1,17 @@
-package petclinic.Configuration;public class HalBrowserSecurityConfiguration {
+package petclinic.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(value = -1)
+public class HalBrowserSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/hal/**").authorizeRequests().anyRequest().permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
 }
